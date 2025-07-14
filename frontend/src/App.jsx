f45b2cc4f313808bf993bea51af43ae70ce97abe
vivek-sahane <viveksahane1528@gmail.com>
2025-07-14 23:16:47 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-amber-400'>Vivek Sahane</div>
+    </>
+  )
+}
+
+export default App
